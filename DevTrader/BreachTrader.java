f683181b72efcd9453a0241d786258159078f2c7
lukas1421 <shijiluke@gmail.com>
2019-06-10 22:29:41 +0800

    6 10 max cross reset to 4

--- DevTrader/BreachTrader.java
+++ DevTrader/BreachTrader.java
@@ -33,7 +33,7 @@ public class BreachTrader implements LiveHandler, ApiController.IPositionHandler
     private static final String HEDGER_INDEX = "MES";
 
     static final int MAX_ATTEMPTS = 100;
-    private static final int MAX_CROSS_PER_MONTH = 2;
+    private static final int MAX_CROSS_PER_MONTH = 4;
     private static final double MAX_ENTRY_DEV = 0.02;
     private static final double MIN_ENTRY_DEV = 0.002;
     private static final double ENTRY_CUSHION = 0.0;
